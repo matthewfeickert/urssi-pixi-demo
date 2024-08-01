ecff0141d20100a815f53df88389c3499ba40e85
Matthew Feickert <matthew.feickert@cern.ch>
2024-08-01 09:40:48 -0500

    fix: Set color cycle from colormap

--- analysis.py
+++ analysis.py
@@ -10,8 +10,12 @@ plots_dir = Path().cwd() / "plots"
 plots_dir.mkdir(exist_ok=True)
 
 # Madicken's favorite :P
-with plt.rc_context({"image.cmap": "jet"}):
+with plt.rc_context(rc={"image.cmap": "jet"}):
     fig, ax = plt.subplots()
+
+    _cmap = plt.get_cmap()
+    ax.set_prop_cycle("color", [_cmap(idx) for idx in np.linspace(0, 1, 10)])
+
     ax.plot(x, y, label=r"$\cos(x)$")
     ax.plot(x + 1, 2 * y, label=r"$2\cos(x+1)$")
     ax.set_xlabel("x")
